2923e5ea8c071440ce3e6916de8c9bd772a44e18
tomas-jeong543 <wjdaudgns03@naver.com>
2026-07-06 16:42:18 +0900

    delete files

--- homework5-31.py
+++
@@ -1,8 +0,0 @@
-
-
-data = bytes.fromhex('050804013C7D7A78287C742B7D3534346A3763626F3B386C6C643F3D6F580050575055045715')
-
-ans = "".join([chr(data[i] ^ (i + 67)) for i in range(38)])
-
-print("ans:")
-print(ans)
